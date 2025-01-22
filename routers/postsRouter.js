5456580310911036fa736360fee01239789ea47a
Aj-Herrera-99 <ajherrera999@gmail.com>
2025-01-22 17:34:51 +0100

    milestone 1 (commit precedente - connessione riuscita) milestone 2: api index corretto e testato

--- routers/postsRouter.js
+++ routers/postsRouter.js
@@ -1,5 +1,7 @@
-const connection = require("../data/db");
 const express = require("express");
 const router = express.Router();
+const { index } = require("../controllers/postsController");
+
+router.get("/", index);
 
 module.exports = router;
